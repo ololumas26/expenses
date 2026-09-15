eb2e66498a38ccfcb0532164ce417af2ad392407
samulolo <eliseufranco26@hotmail.com>
2026-09-16 00:21:03 +0100

    new fix

---
+++ components/success/SuccessContent.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useSearchParams, useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+export default function SuccessContent() {
+  const [message, setMessage] = useState('A carregar...')
+  const params = useSearchParams()
+  const code = params.get('code')
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!code) return
+
+    async function get() {
+      try {
+        const response = await fetch('/api', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ code })
+        })
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
+        const data = await response.json()
+        if (data.code !== 'AUTHORIZED') {
+          setMessage(data?.message ?? 'Não foi possível autentica-lo, tente novamente')
+          return
+        }
+        router.push('/dashboard')
+      } catch (error) {
+        console.log('Erro ao obter resposta: ', error)
+      }
+    }
+    get()
+  }, [code, router])
+
+  return <div>{message}</div>
+}
