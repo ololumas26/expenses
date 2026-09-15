80d6a745604a2d403fe05d0a92b7f6df5e881987
samulolo <eliseufranco26@hotmail.com>
2026-09-15 23:57:31 +0100

    Suspense added in metas

--- app/metas/page.tsx
+++ app/metas/page.tsx
@@ -1,10 +1,16 @@
+import { Suspense } from "react";
+
 import ScreenLayout from "@/components/screens/ScreenLayout";
 import GoalsSummary from "@/components/goals/GoalsSummary";
 import GoalsOverview from "@/components/goals/GoalsOverview";
 
 export default function GoalsPage() {
-    return <ScreenLayout title="Metas" description="Acompanha o progresso dos teus objetivos.">
-        <GoalsSummary />
-        <GoalsOverview />
-    </ScreenLayout>;
+    return 
+        <Suspense>
+             <ScreenLayout title="Metas" description="Acompanha o progresso dos teus objetivos.">
+                <GoalsSummary />
+                <GoalsOverview />
+            </ScreenLayout>;
+        </Suspense>
+
 }
