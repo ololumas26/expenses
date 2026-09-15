12b83d9b7d2fe936c3bf79c40f41940833be8cea
samulolo <eliseufranco26@hotmail.com>
2026-09-16 00:02:27 +0100

    Suspense on meta route fixed

--- app/metas/page.tsx
+++ app/metas/page.tsx
@@ -1,16 +1,15 @@
 import { Suspense } from "react";
-
 import ScreenLayout from "@/components/screens/ScreenLayout";
 import GoalsSummary from "@/components/goals/GoalsSummary";
 import GoalsOverview from "@/components/goals/GoalsOverview";
 
 export default function GoalsPage() {
-    return 
-        <Suspense>
-             <ScreenLayout title="Metas" description="Acompanha o progresso dos teus objetivos.">
+    return (
+        <Suspense fallback={<div className="p-6 text-center text-sm text-tertiary/60">A carregar metas...</div>}>
+            <ScreenLayout title="Metas" description="Acompanha o progresso dos teus objetivos.">
                 <GoalsSummary />
                 <GoalsOverview />
-            </ScreenLayout>;
+            </ScreenLayout>
         </Suspense>
-
+    );
 }
