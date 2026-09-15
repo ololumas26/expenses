4c5cd6a7a82ff45195af7e20b680e25f770172a4
samulolo <eliseufranco26@hotmail.com>
2026-09-16 00:59:14 +0100

    oauth redirect url changed

--- app/actions.ts
+++ app/actions.ts
@@ -8,7 +8,7 @@ export const googleSignIn = async function(){
         const {error, data} = await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: 'http://localhost:3000/success'
+                redirectTo: 'https://expenses-chi-inky.vercel.app/success'
             }
         })
 
